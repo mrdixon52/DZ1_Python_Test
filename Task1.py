762be72149be6a036b49b31de16b77e792f5e4f1
Arseny Zhalnin <mrzhalnin@gmail.com>
2023-01-30 16:54:47 +0300

    Add dop reshenie Task1

--- Task1.py
+++ Task1.py
@@ -6,3 +6,10 @@ while num >= 10:
     num = math.floor(num / 10)
 sum = sum + num
 print(sum)
+
+#Второй способ решения
+# num = input("Enter number: ")
+# sum = 0
+# for i in num:
+#     sum = sum + int(i)
+# print(sum)
